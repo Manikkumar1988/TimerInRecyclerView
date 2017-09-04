02127b872d5b43cb59db4741ed9f036d188d71bc
mani <mani@manis-MacBook-Pro.local>
2017-09-04 10:51:31 +0530

    countdown using handler & runnable

--- app/src/main/java/com/mani/rc/MainActivity.java
+++ app/src/main/java/com/mani/rc/MainActivity.java
@@ -2,51 +2,100 @@ package com.mani.rc;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+  TextView holder;
+  CountDownTimer countDownTimer;
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    final TextView holder = (TextView) findViewById(R.id.timestamp);
+    holder = (TextView) findViewById(R.id.timestamp);
 
 
-    final CountDownTimer countDownTimer = new CountDownTimer(4000, 500) {
-      public void onTick(long millisUntilFinished) {
-        long seconds = millisUntilFinished / 1000;
-        long minutes = seconds / 60;
-        long hours = minutes / 60;
-        long days = hours / 24;
-        String time = days+" "+"days" +" :" +hours % 24 + ":" + minutes % 60 + ":" + seconds % 60;
-        holder.setText(time);
-      }
-
-      public void onFinish() {
-        holder.setText("Time up!");
-      }
-    };
+    countDownTimer = new CustomTimer(10000, 500);
+    final CustomRunnable customRunnable = new CustomRunnable();
 
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
     fab.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null)
-            .show();
-        countDownTimer.start();
 
+        //countDownTimer.start();
+
+        handler.removeCallbacks(customRunnable);
+        customRunnable.millisUntilFinished = 4000; //Current time - received time
+        handler.postDelayed(customRunnable, 100);
       }
     });
   }
 
+  @Override protected void onPause() {
+    super.onPause();
+    //countDownTimer.cancel();
+    handler.removeCallbacksAndMessages(null);
+    holder = null;
+  }
 
+
+  private Handler handler = new Handler();
+
+  public class CustomRunnable implements Runnable {
+
+    public long millisUntilFinished = 40000;
+
+    @Override
+    public void run() {
+      /* do what you need to do */
+      long seconds = millisUntilFinished / 1000;
+      long minutes = seconds / 60;
+      long hours = minutes / 60;
+      long days = hours / 24;
+      String time = days+" "+"days" +" :" +hours % 24 + ":" + minutes % 60 + ":" + seconds % 60;
+      holder.setText(time);
+
+      millisUntilFinished -= 1000;
+
+      Log.d("DEV123",time);
+
+      /* and here comes the "trick" */
+      handler.postDelayed(this, 1000);
+    }
+
+  };
+
+  public class CustomTimer extends CountDownTimer{
+
+    public CustomTimer(long millisInFuture, long countDownInterval) {
+      super(millisInFuture, countDownInterval);
+      Log.d("DEV123","Creating new object");
+    }
+
+    public void onTick(long millisUntilFinished) {
+      long seconds = millisUntilFinished / 1000;
+      long minutes = seconds / 60;
+      long hours = minutes / 60;
+      long days = hours / 24;
+      String time = days+" "+"days" +" :" +hours % 24 + ":" + minutes % 60 + ":" + seconds % 60;
+      holder.setText(time);
+
+      Log.d("DEV123",time);
+    }
+
+    public void onFinish() {
+      holder.setText("Time up!");
+    }
+  }
 }
